6456a7c3eb85c5ff1021fa5d98eef211e4b947d3
KimJoongHyeok <moon960427@gmail.com>
2022-04-13 16:19:58 +0900

    도메인 분석 설계 - 엔티티 클래스 개발 1

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) //꼭 STRING으로 사용해야함, default 는 ORDINAL인데 인덱스처럼 숫자로 들어가게 되서 나중에 중간값이 추가되면 밀려서 다른값이 나오는 오류 발생
+    private DeliveryStatus status; //READY, COMP
+}
