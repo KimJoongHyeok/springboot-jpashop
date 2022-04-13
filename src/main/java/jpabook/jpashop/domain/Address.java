5c53e7103a9ad18d9ab1be7fba26ebd8d059ce62
KimJoongHyeok <moon960427@gmail.com>
2022-04-13 17:16:34 +0900

    도메인 분석 설계 - 엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -11,4 +11,14 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    //JPA 스펙상 생성 , JPA 구현 라이브러리가 객체를 생성할 때 리플렉션,프록시과 같은 기술을 사용할 수 있도록 지원해야하기 때문
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
