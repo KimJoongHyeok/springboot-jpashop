90c009bebee4b8d9a2e22f4181d7943efd202e75
KimJoongHyeok <moon960427@gmail.com>
2022-05-16 22:39:28 +0900

    엔터티 설계시 주의점

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
